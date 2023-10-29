2019fb307c323e2bbde3f52bafbe14e098842091
JOJI <62301319+Jo-Ji-i@users.noreply.github.com>
2023-10-29 15:11:19 +0900

    [level unrated] Title: n보다 커질 때까지 더하기, Time: 0.02 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
@@ -1,9 +1,8 @@
 class Solution {
     public int solution(int[] numbers, int n) {
         int result = 0;
-        for(int i : numbers){
-            result += i;
-            if(result > n) break; 
+        for(int i=0; result <= n; i++){
+            result += numbers[i];
         }
         return result;
     }
