9d1a0554f0ece43ebde4322cbd2d3b6e1d10fc60
JOJI <62301319+Jo-Ji-i@users.noreply.github.com>
2023-11-14 00:21:31 +0900

    [D1] Title: 대각선 출력하기, Time: 98 ms, Memory: 18,224 KB -BaekjoonHub

---
+++ SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.java
@@ -0,0 +1,13 @@
+public class Solution {
+
+    public static void main(String args[]) throws Exception {
+        String s =
+                "#++++\n"+
+                        "+#+++\n"+
+                        "++#++\n"+
+                        "+++#+\n"+
+                        "++++#\n";
+
+        System.out.println(s);
+    }
+}
