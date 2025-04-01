ebf4b157418033530247dbcab9db0744cb7f98fe
jimmiS2 <67740492+jimmiS2@users.noreply.github.com>
2025-04-01 12:16:54 +0900

    [level 0] Title: 배열 만들기 2, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
@@ -1,16 +1,10 @@
 function solution(l, r) {
     const result = [];
-    const empty = [-1];
 
-    for (let i = l; i <= r; i++) {
-        const numArr = Array.from(String(i));
-        let flag = 0;
-        numArr.forEach((element) => {
-            if (element === '0' || element === '5') {
-                flag++;
-            }
-        });
-        if (flag === numArr.length) result.push(i);
+    for (let i = 1; ; i++) {
+        let num = parseInt(i.toString(2)) * 5;
+        if (num > r) break;
+        if (num >= l) result.push(num);
     }
-    return !result.length ? empty : result; 
+    return result.length ? result : [-1];
 }
