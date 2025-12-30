e39cb9a59438bc7d538c9eb6cc3d1f825a35fd75
JOJI <62301319+Jo-Ji-i@users.noreply.github.com>
2025-12-30 23:05:06 +0900

    [Bronze V] Title: A+B - 8, Time: 92 ms, Memory: 9376 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,30 @@
+let fs = require('fs');
+
+const input = fs
+    .readFileSync('/dev/stdin')
+    .toString()
+    .trim()
+    .split(/\s+/)
+    .map(Number);
+
+function solution(item) {
+    const frequency = item[0];
+    let lines = [];
+
+    for (let i = 2; i <= frequency * 2; i += 2) {
+        let total = parseInt(item[i - 1]) + item[i];
+        lines.push(
+            'Case #' +
+                i / 2 +
+                ': ' +
+                item[i - 1] +
+                ' + ' +
+                item[i] +
+                ' = ' +
+                total
+        );
+    }
+    console.log(lines.join('\n'));
+}
+
+solution(input);
