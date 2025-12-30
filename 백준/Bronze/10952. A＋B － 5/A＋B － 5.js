bdb81e7afa9c769109e74a6d2c051a062050444b
JOJI <62301319+Jo-Ji-i@users.noreply.github.com>
2025-12-30 23:39:36 +0900

    [Bronze V] Title: A+B - 5, Time: 92 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.js
@@ -0,0 +1,20 @@
+let fs = require('fs');
+
+const input = fs
+    .readFileSync('/dev/stdin')
+    .toString()
+    .trim()
+    .split(/\s+/)
+    .map(Number);
+
+function solution(item) {
+    let arr = [];
+    let len = item.length - 2;
+
+    for (let i = 0; i < len; i += 2) {
+        arr.push(item[i] + item[i + 1]);
+    }
+    console.log(arr.join('\n'));
+}
+
+solution(input);
